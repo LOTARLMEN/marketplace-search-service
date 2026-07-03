9ed82ce218f1edcdba92f78f54b6dcf7cc3b2b74
VALERIY <val.janushkewitch@yandex.ru>
2026-07-03 15:57:08 +0200

    fix: dynamically select port 8000 when running in Kubernetes

--- bin/api.py
+++ bin/api.py
@@ -4,5 +4,10 @@ from src.fastapi import create_app
 
 app = create_app()
 
+import os
+
 if __name__ == "__main__":
-    uvicorn.run("bin.api:app", host="0.0.0.0", port=8003, reload=True)
+    port = 8003
+    if "KUBERNETES_SERVICE_HOST" in os.environ:
+        port = 8000
+    uvicorn.run("bin.api:app", host="0.0.0.0", port=port, reload=True)
